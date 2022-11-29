3c700819799b1448189f960efd8efe0ab00765bf
80qah <92930382+80qah@users.noreply.github.com>
2022-11-29 21:49:19 +0800

    Update mainWithAverageAlgo.c

--- src/mainWithAverageAlgo.c
+++ src/mainWithAverageAlgo.c
@@ -12,10 +12,11 @@
 int main()
 {
 
-    //intialize stdio
+   //intialize stdio
     stdio_init_all();
     //intialize i2c1
     i2c_init(I2C_PORT, 100*1000);
+    //set i2c for the pins
     gpio_set_function(SCL, GPIO_FUNC_I2C);
     gpio_set_function(SDA, GPIO_FUNC_I2C);
 
@@ -23,6 +24,7 @@ int main()
     gpio_pull_up(SCL);
     gpio_pull_up(SDA);
 
+    //variables to store sum and average of each axis
     int count =0;
     float sumX=0;
     float sumY=0;
@@ -31,48 +33,34 @@ int main()
     float averageY=0;
     float averageZ=0;
 
-    float sumXgyro=0;
-    float sumYgyro=0;
-    float sumZgyro=0;
-    float averageXgyro=0;
-    float averageYgyro=0;
-    float averageZgyro=0;
-
-    int16_t accelerometer[3], gyro[3];
-    MPU6050_Reset();
+    int16_t accelerometer[3]; //array to store accelerometer values
+    MPU6050_Reset(); //reset accelerometer before reading data 
     while(1)
     {
-        MPU6050_ReadData(accelerometer,gyro);
+        MPU6050_ReadData(accelerometer); //call read data function to get readings
+        //add up all the axis values in their respective variable to get the sum of each axis
         sumX+=accelerometer[0];
         sumY+=accelerometer[1];
         sumZ+=accelerometer[2];
 
-        // sumXgyro+=gyro[0];
-        // sumYgyro+=gyro[1];
-        // sumZgyro+=gyro[2];
 
-        count++;
-        if(count==10){
+        count++; //increment count
+        //add up 10 readings of each axis
+        if(count==10){ //when count reaches 10
+            //divide the sum of each axis by 10 to get average
             averageX=sumX/10;
             averageY=sumY/10;
             averageZ=sumZ/10;
-
-            // averageXgyro=sumXgyro/10;
-            // averageYgyro=sumYgyro/10;
-            // averageZgyro=sumZgyro/10;
 
             //Reset all the values
             count =0;
             sumX=0;
             sumY=0;
             sumZ=0;
-            // sumXgyro=0;
-            // sumYgyro=0;
-            // sumZgyro=0;
+
         }
-        printf("Accelerometer   X_OUT= %f   Y_OUT= %f   Z_OUT= %f\r\n",averageX, averageY, averageZ);
-        //printf("Gyro   X_OUT= %f   Y_OUT= %f   Z_OUT= %f\r\n", averageXgyro, averageYgyro, averageZgyro);
-        sleep_ms(500);
+        printf("Accelerometer   X_OUT= %f   Y_OUT= %f   Z_OUT= %f\r\n",averageX, averageY, averageZ); //print the average of 10 readings
+        sleep_ms(500); //delay before new readings
     }
 
     return 0;
