11e7d7f10dfba661ea39a943ff9b4a63dd7b552d
80qah <92930382+80qah@users.noreply.github.com>
2022-11-29 22:54:24 +0800

    Update MPU6050.h

--- inc/MPU6050.h
+++ inc/MPU6050.h
@@ -4,9 +4,9 @@
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 
-#define I2C_PORT i2c1
+#define I2C_PORT i2c1 //define i2c port
 
-#define MPU6050_ADDRESS 0x68
+#define MPU6050_ADDRESS 0x68 //define the address of accelerometer
 
-void MPU6050_Reset();
-void MPU6050_ReadData(int16_t accelerometer[3], int16_t gyro[3]);
+void MPU6050_Reset(); //reset function
+void MPU6050_ReadData(int16_t accelerometer[3]); //read data function
