642c0c810ab0861de9b13ab6b1422cbe79a1d6d2
80qah <92930382+80qah@users.noreply.github.com>
2022-11-29 21:39:36 +0800

    mpu6050 header file

---
+++ accelerometer/inc/MPU6050.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+
+#define I2C_PORT i2c1 //define i2c port
+
+#define MPU6050_ADDRESS 0x68 //define the address of accelerometer
+
+void MPU6050_Reset(); //reset function
+//void MPU6050_ReadData(int16_t accelerometer[3], int16_t gyro[3]);
+void MPU6050_ReadData(int16_t accelerometer[3]); //read data function
